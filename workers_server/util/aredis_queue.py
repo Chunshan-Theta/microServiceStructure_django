56ac3c7d15567dff75af0ad730aa1805299f9a89
gavin mac pro <gavin19950511@gmail.com>
2021-05-11 10:57:38 +0800

    feat(worker): dockerizer

--- workers_server/util/aredis_queue.py
+++ workers_server/util/aredis_queue.py
@@ -8,6 +8,7 @@ import aredis
 class NLUQueue(object):
     def __init__(self, name, namespace='queue', **redis_kwargs):
         # redis的默认参数为：host='localhost', port=6379, db=0， 其中db为定义redis database的数量
+        redis_kwargs["host"] = "redis"
         self.__client = aredis.StrictRedis(**redis_kwargs)
         self.key = '%s:%s' % (namespace, name)
 

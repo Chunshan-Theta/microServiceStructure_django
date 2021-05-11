a333e8fe5160b28484c4a1b74dd89c8cf00bc604
gavin mac pro <gavin19950511@gmail.com>
2021-05-11 10:35:16 +0800

    feat(worker): add uuid for each jobs

--- mysite/service_1/views.py
+++ mysite/service_1/views.py
@@ -1,10 +1,13 @@
+import asyncio
+
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
 from django.http import JsonResponse
-from .util.aredis_queue import NLUQueue
 
+from .util.aredis_queue import NLUQueue
+import uuid
 
 def index(request):
     return HttpResponse("Hello, world. You're at the service_1 index.")
@@ -31,23 +34,37 @@ def hello_world(request):
     })
 
 
-redis_to_MDL = NLUQueue(name="MDL", namespace="common")
-redis_to_API = NLUQueue(name="API", namespace="common")
-
-
 async def worker_test(request):
-    async def __new_work__(item):
-        print(f"new_work: {item}")
-        await redis_to_MDL.enqueue(item)
+    redis_to_MDL = NLUQueue(name="MDL", namespace="common")
+    redis_to_API = NLUQueue(name="API", namespace="common")
 
-    async def __get_responds__():
-        res = await redis_to_API.dequeue_nowait()
+    async def __new_work__(item):
+        request_id = str(uuid.uuid4())
+        print(f"new_work: {item}")
+        await redis_to_MDL.enqueue({
+            "obj": item,
+            "request_id": request_id
+        })
+        return request_id
+
+    async def __get_responds__(request_id: str):
+        res = await redis_to_API.get_msg_by_direct_id(request_id)
         print(f"get_responds: {res}")
         return res
 
     if str(request.method) == "GET":
         get_data = dict(request.GET)
-        await __new_work__(get_data)
-        return JsonResponse({'foo': get_data})
+        request_id = await __new_work__(get_data)
+        worker_response = await __get_responds__(request_id)
+        time = 0
+        while worker_response is None and time < 500:
+            worker_response = await __get_responds__(request_id)
+            time += 1
+            await asyncio.sleep(0.1)
+        else:
+            return JsonResponse({
+                'foo': get_data,
+                "worker_response": str(worker_response)
+            })
     else:
         return HttpResponse("Only support GET method")
--- workers_server/server.py
+++ workers_server/server.py
@@ -1,3 +1,5 @@
+import json
+
 from util.aredis_queue import NLUQueue
 import asyncio
 
@@ -8,20 +10,22 @@ redis_to_API = NLUQueue(name="API", namespace="common")
 async def get_work():
     res = await redis_to_MDL.dequeue_nowait()
     print(f"get_work: {res}")
+    res = json.loads(res.replace("'", "\"")) if res is not None else None
     return res
 
 
-async def send_responds(item):
-    print(f"send_responds: {item}")
-    return await redis_to_API.enqueue(item)
+async def send_responds(obj,request_id):
+    print(f"send_responds: {obj}")
+    return await redis_to_API.set_msg_by_direct_id_ex(id=request_id, second2expire=300, value=obj)
 
 
 async def main():
     while True:
         task = await get_work()
         if task is not None:
-            new_text = "hello "+task
-            await send_responds(new_text)
+            obj = task.get("obj", None)
+            request_id = task.get("request_id", None)
+            await send_responds(obj, request_id)
         else:
             await asyncio.sleep(1)
 
